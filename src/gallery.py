7690570d2e88a626e323291b673ce3a1ca4892c1
toshiakiasakura <aflu.blossompaws@gmail.com>
2021-05-07 10:22:44 +0900

    import contextplt as cplt.

--- src/gallery.py
+++ src/gallery.py
@@ -14,14 +14,14 @@ import seaborn as sns
 import statsmodels.api as sm
 
 # can install by `pip install contextplt`
-import contextplt
+import contextplt as cplt
 
 def simple_scatter():
     anes96 = sm.datasets.anes96
     df = anes96.load_pandas().data
     x = "age"
     y = "logpopul"
-    with contextplt.Single(xlabel=x, ylabel=y, title="scatterplot") as p:
+    with cplt.Single(xlabel=x, ylabel=y, title="scatterplot") as p:
         p.ax.scatter(df[x], df[y], s=1)
 
 def scatter_with_linear_reg():
@@ -29,7 +29,7 @@ def scatter_with_linear_reg():
     df = anes96.load_pandas().data
     x = "age"
     y = "logpopul"
-    with contextplt.Single(xlabel=f"log[{x}]", ylabel=y, 
+    with cplt.Single(xlabel=f"log[{x}]", ylabel=y, 
                            title="scatter with linear regression", xlim=[10, 95]) as p:
         sns.regplot(data=df, x=x, y=y, ax=p.ax, 
                     scatter_kws=dict(s=1, color="purple"), 
@@ -40,7 +40,7 @@ def contourplot():
     df = anes96.load_pandas().data
     x = "age"
     y = "logpopul"
-    with contextplt.Single(figsize=(6,5), title=f"contour plot. {x} and {y}") as p:
+    with cplt.Single(figsize=(6,5), title=f"contour plot. {x} and {y}") as p:
         sns.kdeplot(data=df, x=x, y =y, 
                     common_norm=False, fill=True, ax=p.ax, n_levels=10, 
                     cbar=True, thresh=0, cmap='viridis' )
@@ -53,7 +53,7 @@ def histogram2d():
 
     bins = (20,20)
     rng= ((10,100), (-3, 10))
-    with contextplt.Single(xlabel=x, ylabel=y, title="2D histogram",
+    with cplt.Single(xlabel=x, ylabel=y, title="2D histogram",
                            figsize=(7,5)) as p:
         H =  p.ax.hist2d(df[x], df[y], bins=bins, cmap=plt.cm.jet, 
                          density=True, cmin=0, cmax=None, range=rng)
@@ -65,7 +65,7 @@ def run_stratified_scatter():
     x = "age"
     y = "logpopul"
     c = "educ"
-    with contextplt.Single(xlabel=x, ylabel=y, title="stratified scatter plot") as p:
+    with cplt.Single(xlabel=x, ylabel=y, title="stratified scatter plot") as p:
         stratified_scatter(p.ax, df, x, y, c)
 
 def stratified_scatter(ax, df_: pd.DataFrame, x: str, y: str, c: str) -> None:
@@ -95,7 +95,7 @@ def stacked_histgram():
 
     color_n = len(df[c].unique())
     palette = list(plt.cm.tab10.colors[:color_n])
-    with contextplt.Single() as p:
+    with cplt.Single() as p:
         sns.histplot(data=df,x=x,hue=c, fill=True, palette=palette , alpha=1 )
 
 def kde_density_with_stratification():
@@ -107,7 +107,7 @@ def kde_density_with_stratification():
 
     color_n =  len(df[c].unique())
     palette = list(plt.cm.tab10.colors[:color_n])
-    with contextplt.Single() as p:
+    with cplt.Single() as p:
         sns.kdeplot(data=df, x=x, hue=c, ax=p.ax, 
                     common_norm=False, fill=True, alpha=0.3, bw_adjust=0.5, 
                     palette=palette)
@@ -122,7 +122,7 @@ def kde_density_area_plot():
 
     color_n =  len(df[c].unique())
     palette = list(plt.cm.tab10.colors[:color_n])
-    with contextplt.Single() as p:
+    with cplt.Single() as p:
         sns.kdeplot(data=df, x=x, hue=c, ax=p.ax, 
                     common_norm=True, multiple="fill", fill=True, 
                     bw_adjust=0.5, palette=palette, alpha=1, linewidth=0.1 )
@@ -147,7 +147,7 @@ def stacked_hist_kde_density_and_area_plot_with_stratification():
 
     color_n =  len(df[c].unique())
     palette = list(plt.cm.tab10.colors[:color_n])
-    with contextplt.Multiple(figsize=(6,8), dpi=150,grid=(3,1), label_outer=True,
+    with cplt.Multiple(figsize=(6,8), dpi=150,grid=(3,1), label_outer=True,
                              suptitle="stacked hist., kde density and area plot", 
                              ) as p:
         ax = p.set_ax(1)
