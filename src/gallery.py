05f3b1dd4fba34475f958db33685cf3e7ed85e53
toshiakiasakura <aflu.blossompaws@gmail.com>
2021-05-22 13:00:11 +0900

    function and run part was separeted.

--- src/gallery.py
+++ src/gallery.py
@@ -5,6 +5,9 @@ Dataset explanation can be accessed from here.
 - anes96 : https://www.statsmodels.org/devel/datasets/generated/anes96.html
 
 """
+from typing import Union, Optional, List, Dict, Callable, Any, Tuple
+from types import ModuleType
+
 import pandas as pd
 import numpy as np
 
@@ -16,43 +19,57 @@ import statsmodels.api as sm
 # can install by `pip install contextplt`
 import contextplt as cplt
 
-def simple_scatter():
+def run_simple_scatter():
     anes96 = sm.datasets.anes96
     df = anes96.load_pandas().data
     x = "age"
     y = "logpopul"
+    simple_scatter(df, x, y)
+
+def simple_scatter(df : pd.DataFrame, x : str, y : str) -> None:
     with cplt.Single(xlabel=x, ylabel=y, title="scatterplot") as p:
         p.ax.scatter(df[x], df[y], s=1)
 
-def scatter_with_linear_reg():
+def run_scatter_with_linear_reg():
     anes96 = sm.datasets.anes96
     df = anes96.load_pandas().data
     x = "age"
     y = "logpopul"
+    scatter_with_linear_reg(df, x, y)
+
+def scatter_with_linear_reg(df : pd.DataFrame, x : str, y : str) -> None:
     with cplt.Single(xlabel=f"log[{x}]", ylabel=y, 
                            title="scatter with linear regression", xlim=[10, 95]) as p:
         sns.regplot(data=df, x=x, y=y, ax=p.ax, 
                     scatter_kws=dict(s=1, color="purple"), 
                     line_kws=dict(color="green"))
 
-def contourplot():
+def run_contourplot():
     anes96 = sm.datasets.anes96
     df = anes96.load_pandas().data
     x = "age"
     y = "logpopul"
+    contourplot(df, x, y)
+
+def contourplot(df : pd.DataFrame, x : str, y : str) -> None:
     with cplt.Single(figsize=(6,5), title=f"contour plot. {x} and {y}") as p:
         sns.kdeplot(data=df, x=x, y =y, 
                     common_norm=False, fill=True, ax=p.ax, n_levels=10, 
                     cbar=True, thresh=0, cmap='viridis' )
 
-def histogram2d():
+def run_histogram2d():
     anes96 = sm.datasets.anes96
     df = anes96.load_pandas().data
     x = "age"
     y = "logpopul"
-
     bins = (20,20)
     rng= ((10,100), (-3, 10))
+    histogram2d(df, x, y, bins, rng)
+
+
+def histogram2d(df: pd.DataFrame, x : str, y : str, 
+                bins : Tuple[int,int], rng : Tuple[Tuple[int,int,], Tuple[int,int]]
+                ) -> None:
     with cplt.Single(xlabel=x, ylabel=y, title="2D histogram",
                            figsize=(7,5)) as p:
         H =  p.ax.hist2d(df[x], df[y], bins=bins, cmap=plt.cm.jet, 
@@ -86,25 +103,31 @@ def stratified_scatter(ax, df_: pd.DataFrame, x: str, y: str, c: str) -> None:
         ax.scatter(dfM[x], dfM[y], s=1, label=str(col))
         plt.legend(bbox_to_anchor=(1, 0.98), frameon = False)
 
-def stacked_histgram():
+def run_stacked_histogram():
     anes96 = sm.datasets.anes96
     df = anes96.load_pandas().data
     x = "age"
     y = "logpopul"
     c = "educ"
+    stacked_histogram(df, x, y, c)
 
+def stacked_histogram(df : pd.DataFrame, x : str, y : str, c : str) -> None:
     color_n = len(df[c].unique())
     palette = list(plt.cm.tab10.colors[:color_n])
     with cplt.Single() as p:
         sns.histplot(data=df,x=x,hue=c, fill=True, palette=palette , alpha=1 )
 
-def kde_density_with_stratification():
+def run_kde_density_with_stratification():
     anes96 = sm.datasets.anes96
     df = anes96.load_pandas().data
     x = "age"
     y = "logpopul"
     c = "educ"
+    kde_density_with_stratification(df, x, y, c)
 
+def kde_density_with_stratification(
+        df : pd.DataFrame, x : str, y : str, c : str
+    ) -> None:
     color_n =  len(df[c].unique())
     palette = list(plt.cm.tab10.colors[:color_n])
     with cplt.Single() as p:
@@ -112,14 +135,17 @@ def kde_density_with_stratification():
                     common_norm=False, fill=True, alpha=0.3, bw_adjust=0.5, 
                     palette=palette)
     
-
-def kde_density_area_plot():
+def run_kde_density_area_plot():
     anes96 = sm.datasets.anes96
     df = anes96.load_pandas().data
     x = "age"
     y = "logpopul"
     c = "educ"
+    kde_density_area_plot(df, x, y, c)
 
+def kde_density_area_plot(
+        df : pd.DataFrame, x : str, y : str, c : str
+    ) -> None:
     color_n =  len(df[c].unique())
     palette = list(plt.cm.tab10.colors[:color_n])
     with cplt.Single() as p:
@@ -138,13 +164,17 @@ def move_legend(ax, new_loc="upper left", **kws):
     title = old_legend.get_title().get_text()
     ax.legend(handles, labels, loc=new_loc, title=title, **kws)
 
-def stacked_hist_kde_density_and_area_plot_with_stratification():
+def run_stacked_hist_kde_density_and_area_plot_with_stratification():
     anes96 = sm.datasets.anes96
     df = anes96.load_pandas().data
     x = "age"
     y = "logpopul"
     c = "educ"
+    stacked_hist_kde_density_and_area_plot_with_stratification(df, x, y , c)
 
+def stacked_hist_kde_density_and_area_plot_with_stratification(
+        df : pd.DataFrame, x : str, y : str, c : str,
+    ) -> None:
     color_n =  len(df[c].unique())
     palette = list(plt.cm.tab10.colors[:color_n])
     with cplt.Multiple(figsize=(6,8), dpi=150,grid=(3,1), label_outer=True,
